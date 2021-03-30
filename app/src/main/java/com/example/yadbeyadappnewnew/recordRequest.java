007f416c2535c8c47228df684890fc66c8c688e2
HeliPerlin <heliperlin@gmail.com>
2021-03-30 20:48:33 +0300

    added recording

--- app/src/main/java/com/example/yadbeyadappnewnew/recordRequest.java
+++ app/src/main/java/com/example/yadbeyadappnewnew/recordRequest.java
@@ -1,24 +1,65 @@
 package com.example.yadbeyadappnewnew;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.yadbeyadappnewnew.R;
+
+import java.io.IOException;
 
 public class recordRequest extends AppCompatActivity {
+    private Button play, stop, record;
+    private MediaRecorder myAudioRecorder;
+    private String outputFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_record_request);
+        setContentView(R.layout.activity_main);
+        stop = (Button) findViewById(R.id.send);
+        record = (Button) findViewById(R.id.recordButton);
+        stop.setEnabled(false);
+        play.setEnabled(false);
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+        myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
 
-        Button recordButton = (Button) findViewById(R.id.recordButton);
-        recordButton.setOnClickListener(new View.OnClickListener() {
+        record.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                try {
+                    myAudioRecorder.prepare();
+                    myAudioRecorder.start();
+                } catch (IllegalStateException ise) {
+                    // make something ...
+                } catch (IOException ioe) {
+                    // make something
+                }
+                record.setEnabled(false);
+                stop.setEnabled(true);
             }
         });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAudioRecorder.stop();
+                myAudioRecorder.release();
+                myAudioRecorder = null;
+                record.setEnabled(true);
+                stop.setEnabled(false);
+                play.setEnabled(true);
+            }
+        });
+
     }
 }
