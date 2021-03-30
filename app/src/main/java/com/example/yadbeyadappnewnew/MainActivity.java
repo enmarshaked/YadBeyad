0ea70378db902e30d82f0671079d65581c82ccb5
Enmar Shaked <enmarsh@gmail.com>
2021-03-30 17:42:44 +0300

    added shopping screen

--- app/src/main/java/com/example/yadbeyadappnewnew/MainActivity.java
+++ app/src/main/java/com/example/yadbeyadappnewnew/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.yadbeyadappnewnew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button =(Button) findViewById(R.id.ShoppingBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openShoppingScreen();
+            }
+        });
+    }
+
+    public void openShoppingScreen(){
+        Intent intent = new Intent(this, ShoppingScreen1.class);
+        startActivity(intent);
     }
 }
